81d7286c092df5c9f10d5da931573c7784a7fc23
jchemburkar <jchemburkar@Jonathans-MacBook-Pro.local>
2022-02-06 21:11:34 -0500

    updating uploader to upload patrice bergeron to player table

--- backend/uploader/statsapi/upload_statsapi.py
+++ backend/uploader/statsapi/upload_statsapi.py
@@ -1,6 +1,6 @@
 """ hits endpoints for statsapi and parses data into database """
 import requests
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Optional
 from models.utils import BASE, get_session
 from models.statsapi.player import StatsapiPlayer
 from uploader.schemas.player_schema import RawPlayer, Player
@@ -32,13 +32,31 @@ def transform_data(raw_data: Any, raw_schema: Any, parsed_schema: Any) -> List[D
         # TODO: add logging here
         pass
 
-    raw_data_is_dict = isinstance(raw_data, list)
-    validated_data = raw_schema().load(raw_data, many=raw_data_is_dict)
-    parsed_data = parsed_schema().dump(validated_data, many=raw_data_is_dict)
-    if raw_data_is_dict:
+    raw_data_is_list = isinstance(raw_data, list)
+    validated_data = raw_schema().load(raw_data, many=raw_data_is_list)
+    parsed_data = parsed_schema().dump(validated_data, many=raw_data_is_list)
+    if not raw_data_is_list:
         parsed_data = [parsed_data]
 
     return parsed_data
+
+
+########
+# Load #
+########
+
+def load_data(session: Any, model: Any, data: List, commit_every_n: Optional[int] = 25) -> None:
+    """ insert data to database """
+    BASE.metadata.create_all(tables=[model.__table__], checkfirst=True)
+    for index, row in enumerate(data, start=1):
+        if index % commit_every_n == 0:
+            # TODO: log this with a real logger
+            print(f"Inserted {index}/{len(data)} rows into {model.__tablename__}")
+            session.commit()
+
+        to_merge = model(**row)
+        session.merge(to_merge)
+    session.commit()
 
 
 ##################
@@ -47,8 +65,10 @@ def transform_data(raw_data: Any, raw_schema: Any, parsed_schema: Any) -> List[D
 
 def main():
     """ main func """
+    session = get_session()
     raw_data = extract_statsapi_data("/people/8470638")
     data = transform_data(raw_data["people"], RawPlayer, Player)
+    load_data(session, StatsapiPlayer, data)
 
 
 if __name__ == "__main__":
