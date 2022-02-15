62957c636c40f913b373a0687c9ffecde48092d4
jchemburkar <jchemburkar@Jonathans-MacBook-Pro.local>
2022-02-15 17:07:39 -0500

    adding players relationship to team model; passing through api

--- backend/api/team/schemas.py
+++ backend/api/team/schemas.py
@@ -1,6 +1,31 @@
 """ parses output of query into dict for FE """
 from marshmallow import Schema, fields
 
+class PlayerSchema(Schema):
+    id = fields.Integer()
+    firstName = fields.Str(attribute="first_name")
+    lastName = fields.Str(attribute="last_name")
+    fullName = fields.Str(attribute="full_name")
+    currentTeamId = fields.Integer(attribute="current_team_id")
+    birthDate = fields.Str(attribute="birth_date")
+    birthCity = fields.Str(attribute="birth_city")
+    birthCountry = fields.Str(attribute="birth_country")
+    birthStateProvince = fields.Str(attribute="birth_state_province")
+    height = fields.Integer()
+    weight = fields.Integer()
+    currentAge = fields.Integer(attribute="current_age")
+    isActive = fields.Boolean(attribute="is_active")
+    isAlternate_captain = fields.Boolean(attribute="is_alternate_captain")
+    isCaptain = fields.Boolean(attribute="is_captain")
+    isRookie = fields.Boolean(attribute="is_rookie")
+    link = fields.Str()
+    nationality = fields.Str()
+    primaryNumber = fields.Integer(attribute="primary_number")
+    primaryPosition = fields.Str(attribute="primary_position")
+    rosterStatus = fields.Str(attribute="roster_status")
+    shootsCatches = fields.Str(attribute="shoots_catches")
+
+
 class TeamSchema(Schema):
     abbreviation = fields.Str()
     active = fields.Boolean()
@@ -15,6 +40,7 @@ class TeamSchema(Schema):
     locationName = fields.Str(attribute="location_name")
     name = fields.Str()
     officialSiteUrl = fields.Str(attribute="official_site_url")
+    players = fields.Nested(PlayerSchema(), many=True)
     shortName = fields.Str(attribute="short_name")
     teamName = fields.Str(attribute="team_name")
     venueId = fields.Integer(attribute="venue_id")
